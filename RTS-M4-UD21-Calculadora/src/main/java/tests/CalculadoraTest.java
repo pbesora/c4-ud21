303fac9d265a9d47c5427399a6c6c30e1ca1ad3a
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-08-11 12:51:01 +0200

    JUnit: Import both objects to the testing framework

--- RTS-M4-UD21-Calculadora/src/main/java/tests/CalculadoraTest.java
+++ RTS-M4-UD21-Calculadora/src/main/java/tests/CalculadoraTest.java
@@ -4,8 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import logic.Calculadora;
+import model.Moneda;
+
 class CalculadoraTest {
 
+	Calculadora calculator = new Calculadora(0.0, 0, 1);
+	Moneda[] currencies = {new Moneda(1.0, "Dollar", "US", "USD", '$'), new Moneda(0.9777, "Euro", "Europe", "EUR", '€')};
+	
 	@Test
 	void test() {
 		fail("Not yet implemented");
