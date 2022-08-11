9ecdd077625bac3ca4ee4f008a501e4daa93f590
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-08-11 13:07:24 +0200

    JUnit: Add remaning .toString() test in Moneda() getters

--- RTS-M4-UD21-Calculadora/src/main/java/tests/CalculadoraTest.java
+++ RTS-M4-UD21-Calculadora/src/main/java/tests/CalculadoraTest.java
@@ -22,13 +22,15 @@ class CalculadoraTest {
 		assertEquals(currencies[0].getLugar(), "US");
 		assertEquals(currencies[0].getAbrev(), "USD");
 		assertEquals(currencies[0].getSimbolo(), '$');
-		
+		assertEquals(currencies[0].toString(), ("US - Dollar"));
 		
 		assertEquals(currencies[1].getValor(), 0.9777);
 		assertEquals(currencies[1].getNombre(), "Euro");
 		assertEquals(currencies[1].getLugar(), "Europe");
 		assertEquals(currencies[1].getAbrev(), "EUR");
 		assertEquals(currencies[1].getSimbolo(), '€');
+		assertEquals(currencies[1].toString(), ("Europe - Euro"));
 	}
+
 
 }
